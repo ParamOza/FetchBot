eeae6ed89ffc9719a03192c6e451f95f89dca5e8
bm20894 <milesboz@me.com>
2018-11-10 13:39:00 -0600

    first webhook

--- questions/main.py
+++ questions/main.py
@@ -1,5 +1,16 @@
 '''
 google cloud function thingy
 '''
+
+import wiki, wolf
+from twilio import MessagingResponse
+
+
 def hello(request):
-	return 'Hello World!'
+	if request.method == 'POST':
+		body = request.values.get('Body', None)
+		resp = MessagingResponse()
+
+		msg = resp.message('Hello there. {}'.format(body))
+
+		return str(resp)
