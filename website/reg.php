b15d0c0edebb42cdfd7c1f25fc6454d7a48cebf7
karseyr <karseyrenfert@gmail.com>
2018-11-10 17:53:52 -0600

    halfway

--- website/reg.php
+++ website/reg.php
@@ -1,3 +1,25 @@
 <?php
-    echo file_get_contents(getcwd()."/pagefillins/register.html");
+    $r = $_REQUEST;
+    $stringin = file_get_contents(getcwd()."/pagefillins/register.html");
+
+    $csrf_tok = bin2hex(random_bytes(32));
+    $_SESSION['tok'] = $csrf_tok;
+
+    if($r['plan'] == 'pp'){
+        echo prep($stringin);
+    } else if($r['plan'] == 'sub'){
+        echo sub($stringin);
+    }
+
+    function prep($in){
+        $f = '
+                <form class="w-75 mx-auto light-bg" action="/doreg.php" method="post">
+                    <div class="form-group">
+                        <label>Email </label> <input type="text" name="email" placeholder="user@web.com">
+                    </div>
+                </form>';
+        $stringin = str_replace('{Plan}', 'Prepaid Plan', $stringing);
+        $stringin = str_replace('{Plan}', $f, $stringing);
+    }
+
  ?>
