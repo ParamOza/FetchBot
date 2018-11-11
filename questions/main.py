3226094aaf6044ed51629701922ff5d1b88bcca1
bm20894 <milesboz@me.com>
2018-11-10 21:23:19 -0600

    testing without wolf or wiki sources

--- questions/main.py
+++ questions/main.py
@@ -44,24 +44,28 @@ def hello(request):
 			from_=bot_num
 			)
 
-	if q.lower().startswith('!wiki'):
-		q = command(q)
-		ans = wiki.search(q)
-
-	ans = wolf.respond(q)
-	if not ans:
-		ans = wiki.search(q)
-
-	resp = MessagingResponse()
-
-	if ans['ans']:
-		resp.message('🐶Here\'s what {} said: {}'.format(ans['src'], ans['ans']))
-	else:
-		resp.message('{} couldn\'t find an answer for your question: "{}"' \
+	resp.message('{} couldn\'t find an answer for your question: "{}"' \
 			.format(ans['src'], q))
 
-		resp.message('I\'ll ask my friends if they know the answer. \
-			If they send me one, I\'ll send their response right away!🐶')
-		send_help(q, from_num)
+	resp.message('I\'ll ask my friends if they know the answer. \
+		If they send me one, I\'ll send their response right away!🐶')
+
+	send_help(q, from_num)
+
+	# ans = wolf.respond(q)
+	# if not ans:
+	# 	ans = wiki.search(q)
+
+	# resp = MessagingResponse()
+
+	# if ans['ans']:
+	# 	resp.message('🐶Here\'s what {} said: {}'.format(ans['src'], ans['ans']))
+	# else:
+	# 	resp.message('{} couldn\'t find an answer for your question: "{}"' \
+	# 		.format(ans['src'], q))
+
+	# 	resp.message('I\'ll ask my friends if they know the answer. \
+	# 		If they send me one, I\'ll send their response right away!🐶')
+	# 	send_help(q, from_num)
 
 	return str(resp)
