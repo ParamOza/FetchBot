e2c44dceee2f0d0d8b22a04a1563ae8eeb019a23
karseyr <karseyrenfert@gmail.com>
2018-11-11 11:27:08 -0600

    Rate limiting

--- public/respond.php
+++ public/respond.php
@@ -1,4 +1,19 @@
 <?php
+    session_start();
+    if(array_key_exists('askedalot', $_SESSION)){
+        $_SESSION['askedalot'] += 1;
+    } else {
+        $_SESSION['askedalot'] = 1;
+        $_SESSION['first'] = strtotime('+10 minutes');
+    }
+
+    if(($_SESSION['first'] - time()) < 0){
+        $_SESSION['askedalot'] = 1; // Whatever.
+    }
+    if($_SESSION['askedalot'] > 6){
+        echo 'Calm down.  Only 6 questions per 10 minutes allowed on the website!';
+        exit;
+    }
 
     $r = urlencode($_REQUEST['q']);
     $ch = curl_init();
