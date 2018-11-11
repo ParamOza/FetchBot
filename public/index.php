ac72fe007a4f8486e02fb5b7cc04189f1203410c
karseyr <karseyrenfert@gmail.com>
2018-11-11 11:00:10 -0600

    Hi hey hello

--- public/index.php
+++ public/index.php
@@ -134,7 +134,7 @@
 
     <div class="modal fade" id="displaymod" tabindex="-1">
       <div class="modal-dialog" role="document">
-        <div class="modal-content">
+        <div class="modal-content text-center">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">We have an answer:</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -148,7 +148,6 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
