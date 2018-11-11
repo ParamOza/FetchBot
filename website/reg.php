79d6aae425fba98f6d1e64cde46ca45346ae1903
karseyr <karseyrenfert@gmail.com>
2018-11-10 18:29:32 -0600

    Upgraded Form

--- website/reg.php
+++ website/reg.php
@@ -16,17 +16,17 @@
                 <form class="w-75 mx-auto mt-3 p-3 light-bg text-light" action="/doreg.php" method="post">
                     <input type="hidden" name="csrf" value="'.$_SESSION['tok'].'">
 
-                    <div class="form-group row">
+                    <div class="form-group row my-2">
                         <label class="ml-auto mr-2">Phone Number </label>
                         <input class="form-control col-5 ml-2 mr-auto" type="text" name="phone" placeholder="1 (555)324-6172">
                     </div>
 
-                    <div class="form-group row">
+                    <div class="form-group row my-2">
                         <label class="ml-auto mr-2">Email </label>
                         <input class="form-control col-6 ml-2 mr-auto" type="text" name="email" placeholder="user@web.com">
                     </div>
 
-                    <div class="form-group row">
+                    <div class="form-group row my-2">
                         <label class="ml-auto mr-2">Password </label>
                         <input class="form-control col-6 ml-2 mr-auto" type="passsword" name="passwd">
                     </div>
