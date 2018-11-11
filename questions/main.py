cb153ab2ca21c51ececfe6d8d63e51ad18c4bafa
bm20894 <milesboz@me.com>
2018-11-11 02:43:26 -0600

    added personality features

--- questions/main.py
+++ questions/main.py
@@ -4,17 +4,37 @@ google cloud function thingy
 
 import wiki, wolf
 from twilio.twiml.messaging_response import MessagingResponse
-# from twilio.twiml.voice_response import VoiceResponse
 from twilio.rest import Client
 from send_help import send_help
 import issues
 import re, os
+import requests, random
 
 bot_num = '+15743558881'
 
 def command(s):
 	return ' '.join(s.split()[1:])
 
+def handle_personality(q):
+
+	joke = requests.get('https://geek-jokes.sameerkumar.website/api') \
+	.text.strip()[1:-1]
+
+	personal_questions = {
+		'what is love': 'Baby don\'t hurt me',
+		'how are you': 'I\'m {}!'.format(random.choice( \
+			['fantastic', 'stellar', 'joyous', 'stupendous'])),
+		'who made you': 'I was created by Miles Boswell.👨‍💻',
+		'open the pod bay doors': 'I\'m sorry Dave, I\'m afraid I can\'t do that.',
+		'tell me a joke': 'OK, here\'s one I like:\n"{}"'.format(joke),
+		'wow': 'I know, right??'
+	}
+
+	for personal_q in personal_questions:
+		if q.startswith(personal_q):
+			return personal_questions[personal_q]
+	else:
+		return
 
 def hello(request):
 	q = request.values.get('Body', None)
@@ -29,39 +49,42 @@ def hello(request):
 			from_num = '+{}'.format(mat.group(1))
 			break
 
-	if q.lower() == 'what is love':
-		resp.message('Baby don\'t hurt me, no more..🐶')
+	personal_q = handle_personality(q.lower())
+	if personal_q:
+		resp.message(personal_q)
 		return str(resp)
 
 
-	# "!ans The 1st element is Hydrogen."
-	if q.lower().startswith('!ans'): # check if admin
-		admin_response = command(q)
-		print(admin_response)
+	# ---------------ADMIN-RESPONSE-------------
 
-		# resolve issue
-		admin_num = from_num
-		recent_from_bot = client.messages.list(
-				from_=bot_num,
-				to=admin_num
-				)
-		resolved_id = recent_from_bot[0].body.split('#')[1]
+	# # "!ans The 1st element is Hydrogen."
+	# if q.lower().startswith('!ans'): # check if admin
+	# 	admin_response = command(q)
+	# 	print(admin_response)
 
-		# get entry from DATABASE
-		entry = issues.resolve(resolved_id)
-		print(entry)
-		if entry:
-			resp.message('Thanks for your help!🐶')
-			_, issue, ask_num = entry
+	# 	# resolve issue
+	# 	admin_num = from_num
+	# 	recent_from_bot = client.messages.list(
+	# 			from_=bot_num,
+	# 			to=admin_num
+	# 			)
+	# 	resolved_id = recent_from_bot[0].body.split('#')[1]
 
-			# send answer back to asker
-			follow_up = '"{}"\n👨‍💻Admin Response:\n{}'.format(issue, admin_response)
-			msg_response = client.messages.create(
-				body=follow_up,
-				to=ask_num,
-				from_=bot_num
-				)
-			return str(resp)
+	# 	# get entry from DATABASE
+	# 	entry = issues.resolve(resolved_id)
+	# 	print(entry)
+	# 	if entry:
+	# 		resp.message('Thanks for your help!🐶')
+	# 		_, issue, ask_num = entry
+
+	# 		# send answer back to asker
+			# follow_up = '"{}"\n👨‍💻Admin Response:\n{}'.format(issue, admin_response)
+	# 		msg_response = client.messages.create(
+	# 			body=follow_up,
+	# 			to=ask_num,
+	# 			from_=bot_num
+	# 			)
+	# 		return str(resp)
 
 
 	ans = wolf.respond(q)
@@ -69,10 +92,10 @@ def hello(request):
 		ans = wiki.search(q)
 
 	if ans['ans']:
-		resp.message('🐶Here\'s what {} said: {}'.format(ans['src'], ans['ans']))
+		resp.message('🐶Here\'s what {} said: "{}"'.format(ans['src'], ans['ans']))
 	else:
-		resp.message('{} couldn\'t find an answer for your question: "{}"' \
-			.format(ans['src'], q))
+		resp.message('Sorry, I couldn\'t find an answer for your question: "{}"' \
+			.format(q))
 
 		resp.message('I\'ll ask my friends if they know the answer. \
 			If they send me one, I\'ll send their response right away!🐶')
--- questions/test_main.py
+++ questions/test_main.py
@@ -1,17 +1,16 @@
 '''
 google cloud function thingy
 '''
-
-import os, re
-from flask import Flask, request
+import wiki, wolf
 from twilio.twiml.messaging_response import MessagingResponse
+# from twilio.twiml.voice_response import VoiceResponse
 from twilio.rest import Client
 from send_help import send_help
 import issues
+import re, os
+import requests, random
 
-
-client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-
+from flask import Flask, request
 app = Flask(__name__)
 
 bot_num = '+15743558881'
@@ -19,11 +18,36 @@ bot_num = '+15743558881'
 def command(s):
 	return ' '.join(s.split()[1:])
 
+def handle_personality(q):
+
+	joke = requests.get('https://geek-jokes.sameerkumar.website/api') \
+	.text.strip()[1:-1]
+
+	personal_questions = {
+		'what is love': 'Baby don\'t hurt me',
+		'how are you': 'I\'m {}!'.format(random.choice( \
+			['fantastic', 'stellar', 'joyous', 'stupendous'])),
+		'who made you': 'I was created by Miles Boswell.👨‍💻',
+		'open the pod bay doors': 'I\'m sorry Dave, I\'m afraid I can\'t do that.',
+		'tell me a joke': 'OK, here\'s one I like:\n"{}"'.format(joke),
+		'wow': 'I know, right??'
+	}
+
+	if 'siri' in q:
+		return 'Siri and I are close friends, but we all know who\'s better.🤷‍♂️'
+
+	for personal_q in personal_questions:
+		if q.startswith(personal_q):
+			return personal_questions[personal_q]
+	else:
+		return
+
 @app.route('/sms')
 def hello():
 	q = request.values.get('Body', None)
 	url = request.url
 	resp = MessagingResponse()
+	client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
 
 
 	for param in url.split('&'):
@@ -32,59 +56,57 @@ def hello():
 			from_num = '+{}'.format(mat.group(1))
 			break
 
-	# if response from admin
-	# "!ans The 1st element is Hydrogen."
-	if q.lower().startswith('!ans'): # check if admin
-		admin_response = command(q)
-		print(admin_response)
+	personal_q = handle_personality(q.lower())
+	if personal_q:
+		resp.message(personal_q)
+		return str(resp)
 
-		# resolve issue
-		client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-		admin_num = from_num
-		recent_from_bot = client.messages.list(
-				from_=bot_num,
-				to=admin_num
-				)
-		resolved_id = recent_from_bot[0].body.split('#')[1]
+	# ---------------ADMIN-RESPONSE-------------
 
-		entry = issues.resolve(resolved_id)
-		print(entry)
-		if entry:
-			resp.message('Thanks for your help!🐶')
-			_, issue, ask_num = entry
+	# # "!ans The 1st element is Hydrogen."
+	# if q.lower().startswith('!ans'): # check if admin
+	# 	admin_response = command(q)
+	# 	print(admin_response)
 
-			# send answer back to asker
-			follow_up = '"{}"\n👨‍💻Admin Response:\n{}'.format(issue, admin_response)
-			msg_response = client.messages.create(
-				body=follow_up,
-				to=ask_num,
-				from_=bot_num
-				)
-			return str(resp)
+	# 	# resolve issue
+	# 	admin_num = from_num
+	# 	recent_from_bot = client.messages.list(
+	# 			from_=bot_num,
+	# 			to=admin_num
+	# 			)
+	# 	resolved_id = recent_from_bot[0].body.split('#')[1]
 
+	# 	# get entry from DATABASE
+	# 	entry = issues.resolve(resolved_id)
+	# 	print(entry)
+	# 	if entry:
+	# 		resp.message('Thanks for your help!🐶')
+	# 		_, issue, ask_num = entry
 
-	resp.message('couldn\'t find an answer for your question: "{}"' \
-			.format(q))
+	# 		# send answer back to asker
+			# follow_up = '"{}"\n👨‍💻Admin Response:\n{}'.format(issue, admin_response)
+	# 		msg_response = client.messages.create(
+	# 			body=follow_up,
+	# 			to=ask_num,
+	# 			from_=bot_num
+	# 			)
+	# 		return str(resp)
 
-	send_help(q, from_num)
+	resp.message('🤔')
 
+	ans = wolf.respond(q)
+	if not ans:
+		ans = wiki.search(q)
 
-	# resp.message('I\'ll ask my friends if they know the answer. \
-	# 	If they send me one, I\'ll send their response right away!🐶')
+	if ans['ans']:
+		resp.message('🐶Here\'s what {} said: {}'.format(ans['src'], ans['ans']))
+	else:
+		resp.message('{} couldn\'t find an answer for your question: "{}"' \
+			.format(ans['src'], q))
 
-	# ans = wolf.respond(q)
-	# if not ans:
-	# 	ans = wiki.search(q)
-
-	# if ans['ans']:
-	# 	resp.message('🐶Here\'s what {} said: {}'.format(ans['src'], ans['ans']))
-	# else:
-	# 	resp.message('{} couldn\'t find an answer for your question: "{}"' \
-	# 		.format(ans['src'], q))
-
-	# 	resp.message('I\'ll ask my friends if they know the answer. \
-	# 		If they send me one, I\'ll send their response right away!🐶')
-	# 	send_help(q, from_num)
+		resp.message('I\'ll ask my friends if they know the answer. \
+			If they send me one, I\'ll send their response right away!🐶')
+		# PLACE ISSUE IN DATABASE or summin
 
 	return str(resp)
 
