ef5246de42bc7d3fe0ba0bcca619583b56c1bb26
karseyr <karseyrenfert@gmail.com>
2018-11-10 18:07:15 -0600

    Updates to form

--- website/reg.php
+++ website/reg.php
@@ -13,10 +13,22 @@
 
     function prepStr($in){
         $f = '
-                <form class="w-75 mx-auto light-bg" action="/doreg.php" method="post">
+                <form class="w-75 mx-auto mt-3 p-3 light-bg" action="/doreg.php" method="post">
+                    <input type="hidden" name="csrf" value="'.$_SESSION['tok'].'">
+                    
                     <div class="form-group">
-                        <input type="hidden" name="csrf" value="'.$_SESSION['tok'].'">
-                        <label>Email </label> <input type="text" name="email" placeholder="user@web.com">
+                        <label>Phone Number </label>
+                        <input class="form-control" type="text" name="phone" placeholder="1 (555)324-6172">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Email </label>
+                        <input class="form-control" type="text" name="email" placeholder="user@web.com">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password </label>
+                        <input class="form-control" type="passsword" name="passwd">
                     </div>
                 </form>';
         $in = str_replace('{Plan}', 'a Prepaid Plan', $in);
