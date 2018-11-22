1f9539fe424f32ed3933800257e719c6d73c941e
karseyr <karseyrenfert@gmail.com>
2018-11-21 22:11:44 -0600

    Fixed error where ans is null (Sorry, None)

--- public/js/typing.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#questionOutput").typed({
-        strings: ["What words rhyme with orange?", "How long does it take to walk around the world?", "What is the electronegativity of carbon?", "What is the derivative of eˣ?", "Define avocado.", "When did Epcot open?", "States that cross the I-95.", "Atomic mass of mercury", "Lindbergh solo flight cities involved.", "Cowboys at Steelers in 1988.", "Average hit points of Champion level Digimon vs. Mega level Digimon", "What is the Wizard of Oz?", "How many dog breeds are there?",
-        "What is the probability 5 people were born on the same day of the week?", "Arc length of polar curve r=t*sin(t) from t=2 to t=6", "$10,000 current US dollars in 1910", "How many books does the new york public library have?"],
-        attr: "placeholder",
-        typeSpeed: 40,
-        loop: true
-    });
-});
--- questions/responses.py
+++ questions/responses.py
@@ -46,7 +46,10 @@ def get_answer(q):
 	if not ans:
 		ans = wiki.search(q)
 
-	if ans['ans']:
+	if ans is None:
+		resp = 'Sorry, I\'m having trouble getting you an answer for your question: "{}"' \
+			.format(q)
+	elif ans['ans']:
 		resp = '🐶Here\'s what {} said: {}'.format(ans['src'], ans['ans'])
 	else:
 		resp = '{} couldn\'t find an answer for your question: "{}"' \
